c7a6f59e257ae3c9b0d6605818234e4b2c52cc5f
Mati-IR <maatpaab@gmail.com>
2022-04-13 11:31:55 +0200

    Basic board GUI with buttons

--- src/com/company/board.java
+++ src/com/company/board.java
@@ -13,22 +13,51 @@ public class board {
     public static final int G = 7;
     public static final int H = 8;
 
+    private static final Color myBrown = new Color(143, 90, 10);
+    private static final Color myLtBrown = new Color(245, 200, 144);
     int size = 8;//x and y dimension of board
     int fields[][] = new int[size][size];//2D array of size x size
 
-    void initialize(){
-        JFrame frame = new JFrame();
-        JPanel panel = new JPanel();
-        frame.setTitle("Russian checkers :)");
-        frame.add(panel, BorderLayout.CENTER);
+    void draw() {
+
+        JFrame frame = new JFrame("Checkers");//        setting up game window
+        frame.setSize(620, 810);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setSize(500, 500);
+        frame.setLayout(new BoxLayout(frame.getContentPane(), BoxLayout.Y_AXIS));
+
+        JPanel panel = new JPanel(); //        setting up checkers board
+        panel.setSize(100,100);
+        panel.setLayout(new GridLayout(size, size));
+        panel.setBorder(BorderFactory.createEmptyBorder(10, 10, 200, 10));
+
+        for (int i = 0; i < size; i++) { //     adding fields of checkers board
+            for (int j = 0; j < size; j++) {
+                JButton button = new JButton();
+                if (i%2 == 0) {
+                    if (j%2 == 0) {
+                        button.setBackground(myLtBrown);
+                    }else {
+                        button.setBackground(myBrown);
+                    }
+                }else {
+                    if (j%2 == 0) {
+                        button.setBackground(myBrown);
+                    }else {
+                        button.setBackground(myLtBrown);
+                    }
+                }
+                panel.add(button);
+            }
+        }
+
         frame.setVisible(true);
-
-        panel.setBorder(BorderFactory.createEmptyBorder(10, 10, 10, 10));
-        panel.setLayout(new GridLayout(0,1));
-
+        frame.add(panel);       //include checkers board in game window
+        panel.setVisible(true);
 
 
     }
+
+    void initialize(){
+        draw();
+         }
 }
