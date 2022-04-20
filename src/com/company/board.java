5c5135892961b237ac59c821a438352f7a75aa68
Mati-IR <maatpaab@gmail.com>
2022-04-20 19:48:53 +0200

    added choice menu

--- src/com/company/board.java
+++ src/com/company/board.java
@@ -2,28 +2,33 @@ package com.company;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import static java.lang.Thread.sleep;
 
 public class board {
-    public static final int A = 1;
-    public static final int B = 2;
-    public static final int C = 3;
-    public static final int D = 4;
-    public static final int E = 5;
-    public static final int F = 6;
-    public static final int G = 7;
-    public static final int H = 8;
+    int gameType = -1;//incorrect value for used in do while loop in choosegameType()
+    private final int A = 1;
+    private final int B = 2;
+    private final int C = 3;
+    private final int D = 4;
+    private final int E = 5;
+    private final int F = 6;
+    private final int G = 7;
+    private final int H = 8;
 
     private static final Color myBrown = new Color(143, 90, 10);
     private static final Color myLtBrown = new Color(245, 200, 144);
-    int size = 8;//x and y dimension of board
+    private int size = 8;//x and y dimension of board
     JButton fieldsVisual[][] = new JButton[size][size];//2D array for GUI
 
-    int fieldsValues[][] = new int[size][size];//2D array for backend depiction of field
-    int empty = 0;
-    int darkPawn = 1;
-    int lightPawn = 2;
+    private int fieldsValues[][] = new int[size][size];//2D array for backend depiction of field. It portrays placement of pawns on the board
+    private final int empty = 0;
+    private final int darkPawn = 1;
+    private final int lightPawn = 2;
 
-    int initialFieldValue(int xAxis, int yAxis) {
+    private int initialFieldValue(int xAxis, int yAxis) {
         //0 dark_empty.png
         //1 light_empty.png
         //2 dark_darkpawn.png
@@ -42,7 +47,7 @@ public class board {
         return xAxis%2 != yAxis%2 ? 1 : 3;//bottom fields, light pawns on start
     }
 
-    void drawBoard() {
+    private void drawBoard() {
 
         JFrame frame = new JFrame("Checkers");//        setting up game window
         frame.setSize(620, 600);
@@ -99,9 +104,83 @@ public class board {
 
     }
 
+    int getSize(){
+        return size;
+    }
+    int getGameType(){
+        return gameType;
+    }
+
+    private void choosegameType(){
+        //set up choice window
+        JFrame frame = new JFrame("Choose game type");
+        frame.setSize(400, 100);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setLayout(new FlowLayout());
+
+        JLabel label = new JLabel("Choose game type:");
+
+        //setup buttons
+        JButton choice0 = new JButton("Hotseat");
+        JButton choice1 = new JButton("LAN");
+        JButton choice2 = new JButton("PvE");
+
+        //add action listeners
+        choice0.addActionListener(new ActionListener() {
+            @Override public void actionPerformed(ActionEvent e) {
+                gameType = 0;
+                frame.dispose();
+            }
+        });
+        choice1.addActionListener(new ActionListener() {
+            @Override public void actionPerformed(ActionEvent e) {
+                gameType = 1;
+                frame.dispose();
+            }
+        });
+        choice2.addActionListener(new ActionListener() {
+            @Override public void actionPerformed(ActionEvent e) {
+                gameType = 2;
+                frame.dispose();
+            }
+        });
+
+        //add buttons to frame
+        frame.add(label);
+        frame.add(choice0);
+        frame.add(choice1);
+        frame.add(choice2);
 
 
-    void initialize(){
+        frame.setVisible(true);
+
+        do {
+            System.out.print("");//without this statement program will not see user input
+        }while(getGameType() == -1);//wait for input
+
+        return;
+    }
+
+    private void hotseatGame(){
+
+    }
+
+    public void initialize(){
         drawBoard();         //open window with board GUI
     }
+    public void play(){
+        choosegameType();
+        System.out.println("gameType = " + getGameType());
+        /*
+        0 - hotseat
+        1 - LAN
+        2 - PvE
+         */
+        switch (getGameType()){
+            case 0:{
+                hotseatGame();
+            }
+        }
+    }
+
 }
